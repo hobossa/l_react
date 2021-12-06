0155c52db6536edd8546c2cd063911975b444c92
hobossa <hoboss@gmail.com>
2021-12-06 01:26:23 -0800

    State, Action, and Reducers

---
+++ redux-fundamentals-example-app/src/features/filters/filtersSlice.js
@@ -0,0 +1,20 @@
+const initialState = {
+    status: 'All',
+    colors: []
+}
+
+
+export default function filtersReducer(state = initialState, action) {
+    switch (action.type) {
+        case 'filters/statusFilterChanged': {
+            return {
+                // copy the other filter fields
+                ...state.filters,
+                // And replace the status field with the new value
+                status: action.payload
+            }
+        }
+        default:
+            return state
+    }
+}
